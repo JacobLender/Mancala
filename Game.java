7b821510b5d4bb9e2df7f84c9cf92583f2e5f79a
Jamsheedy <58054056+Jamsheedy@users.noreply.github.com>
2020-03-27 19:26:53 -0400

    Test Commit...

--- Game.java
+++ Game.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class Game {
     private Board GameBoard = new Board();
     // add other objects such as players
+    // testing commit from IDE
     public static void main(String [] args){
         JFrame frame = new JFrame("Mancala");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
