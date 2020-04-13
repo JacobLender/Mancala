44345bdb84e88a171186c70f9f87884ea91a171d
Jake Lender <jakelender@gmail.com>
2020-04-13 15:18:15 -0400

    Basic Graphics

--- menu.java
+++ menu.java
@@ -1,35 +1,64 @@
 import javax.swing.*;
 import javax.swing.text.JTextComponent;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JCheckBox;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+
 
 public class menu extends JFrame{
-//    private JLabel label1;
-//    private JButton start;
-//    private JButton Rules;
-//    private JButton Options;
-//    private JButton Stats;
+    //    private JLabel label1;
+    private JButton start;
+    private JButton Rules;
+    private JButton Options;
+    private JButton Stats;
+    private String gameMode;
+    private String playerOption;
     private static int playCount;
     private static int gamesWon;
     private static double winPercentage;
+    private int colorChooser;
+    private final Color colorNames[] = { Color.BLACK, Color.BLUE, Color.CYAN,
+            Color.GRAY, Color.GREEN, Color.MAGENTA, Color.ORANGE,
+            Color.PINK, Color.RED, Color.YELLOW };
 
+    public menu() {
+        super();
+        this.setSize(100, 100);
+        this.setVisible(true);
 
+        createAndShowGUI("Menu");
+        //createAndShowGUI("Setup");
 
-    private static void addAButton(String buttName, Container container){
-        JButton button = new JButton(buttName);
-        button.setAlignmentX(Component.CENTER_ALIGNMENT);
-        button.setFont(new Font("Verdana", Font.PLAIN, 50));
-        button.setPreferredSize(new Dimension(400, 75));
-        container.add(button);
-    }
-    private static void addRadioButton(String text, Container container){
-        JRadioButton radioButt = new JRadioButton(text);
-        radioButt.setAlignmentX(Component.CENTER_ALIGNMENT);
-        radioButt.setFont(new Font("Verdana", Font.PLAIN, 20));
-        radioButt.setPreferredSize(new Dimension(50, 75));
-        container.add(radioButt);
+        //createAndShowGUI("Rules");
     }
 
-    private static void addlabel(String text, Container container, int size, Color col){
+    public static void main(String[] args) {
+        menu men = new menu();
+        men.setVisible(true);
+//        createAndShowGUIMenu();
+//        createAndShowGUISetup();
+//        createAndShowGUIStats();
+//        createAndShowGUIRules();
+    }
+
+    private void addAButton(JButton buttName, Container container) {
+        buttName.setAlignmentX(Component.CENTER_ALIGNMENT);
+        buttName.setFont(new Font("Verdana", Font.PLAIN, 50));
+        buttName.setPreferredSize(new Dimension(400, 75));
+        container.add(buttName);
+    }
+
+    private static void addRadioButton(JRadioButton radio, Container container) {
+        radio.setAlignmentX(Component.CENTER_ALIGNMENT);
+        radio.setFont(new Font("Verdana", Font.PLAIN, 20));
+        radio.setPreferredSize(new Dimension(50, 75));
+        container.add(radio);
+    }
+
+    private static void addlabel(String text, Container container, int size, Color col) {
         JLabel label = new JLabel(text);
         label.setAlignmentX(Component.CENTER_ALIGNMENT);
         label.setFont(new Font("Verdana", Font.BOLD, size));
@@ -37,7 +66,7 @@ public class menu extends JFrame{
         container.add(label);
     }
 
-    private static void addlabelforStats(String text, double measure, Container container, int size, Color col){
+    private static void addlabelforStats(String text, double measure, Container container, int size, Color col) {
         JLabel labelFStats = new JLabel(text + measure);
         labelFStats.setAlignmentX(Component.CENTER_ALIGNMENT);
         labelFStats.setFont(new Font("Verdana", Font.BOLD, size));
@@ -45,35 +74,55 @@ public class menu extends JFrame{
         container.add(labelFStats);
     }
 
-    public static void Menu(Container pane){
+    public void Menu(Container pane) {
         pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+        ButtonHandler buttonHandle = new ButtonHandler();
 
-        addlabel("MANCALA", pane, 100, Color.BLUE);
-        addAButton("Play Game", pane);
-        addAButton("Statistics", pane);
-        addAButton("DO NOT PRESS!!!", pane);
+        addlabel("MANCALA", pane, 100, colorNames[colorChooser]);
+        JButton playGame = new JButton("Play Game");
+        addAButton(playGame, pane);
+        playGame.addActionListener(buttonHandle);
+        JButton stats = new JButton("Statistics");
+        addAButton(stats, pane);
+        stats.addActionListener(buttonHandle);
+        JButton DoNotPress = new JButton("DO NOT PRESS!");
+        addAButton(DoNotPress, pane);
+        DoNotPress.addActionListener(buttonHandle);
 
     }
-    public static void Setup(Container pane){
+
+    public void Setup(Container pane) {
         pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+        CheckBoxHandler Players = new CheckBoxHandler();
 
         addlabel("Choose Players", pane, 50, Color.BLACK);
-        addRadioButton("Player vs Player", pane);
-        addRadioButton("Player vs Computer", pane);
-        addRadioButton("Computer vs Computer", pane);
+        JRadioButton PVP = new JRadioButton("Player vs Player");
+        addRadioButton(PVP, pane);
+        PVP.addActionListener(Players);
+        JRadioButton PVC = new JRadioButton("Player vs Computer");
+        addRadioButton(PVC, pane);
+        PVC.addActionListener(Players);
+        JRadioButton CVC = new JRadioButton("Computer vs Computer");
+        addRadioButton(CVC, pane);
+        CVC.addActionListener(Players);
 
+        CheckBoxHandler gameMode = new CheckBoxHandler();
         addlabel("Game Mode", pane, 50, Color.BLACK);
-        addRadioButton("Classic", pane);
-        addRadioButton("Flash", pane);
-        addRadioButton("Avalanche", pane);
+        JRadioButton capture = new JRadioButton("Capture");
+        addRadioButton(capture, pane);
+        capture.addActionListener(gameMode);
+        JRadioButton avalanche = new JRadioButton("Avalanche");
+        addRadioButton(avalanche, pane);
+        capture.addActionListener(gameMode);
 
-        addAButton("Start Game", pane);
-
+        ButtonHandler buttonHandle = new ButtonHandler();
+        JButton startGame = new JButton("Start Game");
+        addAButton(startGame, pane);
+        startGame.addActionListener(buttonHandle);
 
     }
 
-    public static void Statistics(Container pane){
-        JPanel panel = new JPanel();
+    public static void Statistics(Container pane) {
         pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 
         addlabel("Statistics", pane, 80, Color.RED);
@@ -83,18 +132,18 @@ public class menu extends JFrame{
 
     }
 
-    public static void Rules(Container pane){
-        String label = ("<html><span>1. Each Player has a store on one side of the board.<br><br>"+
-                        "2.Players take turns choosing a pile from one of the holes. Moving<br>"+
-                        "counter-clockwise, stones from the selected pile are deposited in each<br>"+
-                        "of the following hole until you run out of stones.<br><br>"+
-                        "3. If you drop the last stone into your store - you get a free turn.<br><br>"+
-                        "4. If you drop the last stone into an empty hole on your side of the board"+
-                        "- you can capture stones from the hole on the opposite side.<br><br>"+
-                        "5. The game ends when all six holes on either side of the board are empty."+
-                        "If a player has any stones on their side of the board when the game ends -"+
-                        "he will capture all of those stones.<br><br><br>"+
-                        "GOAL: PLAYER WITH THE MOST STONES IN THEIR STORE WINS<br></span></html>");
+    public static void Rules(Container pane) {
+        String label = ("<html><span>1. Each Player has a store on one side of the board.<br><br>" +
+                "2.Players take turns choosing a pile from one of the holes. Moving<br>" +
+                "counter-clockwise, stones from the selected pile are deposited in each<br>" +
+                "of the following hole until you run out of stones.<br><br>" +
+                "3. If you drop the last stone into your store - you get a free turn.<br><br>" +
+                "4. If you drop the last stone into an empty hole on your side of the board" +
+                "- you can capture stones from the hole on the opposite side.<br><br>" +
+                "5. The game ends when all six holes on either side of the board are empty." +
+                "If a player has any stones on their side of the board when the game ends -" +
+                "he will capture all of those stones.<br><br><br>" +
+                "GOAL: PLAYER WITH THE MOST STONES IN THEIR STORE WINS<br></span></html>");
 
         pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
         addlabel("Rules of the Game", pane, 50, Color.RED);
@@ -102,54 +151,88 @@ public class menu extends JFrame{
 
     }
 
-    private static void createAndShowGUIMenu(){
-        JFrame frame = new JFrame("GAME MENU");
+    private void createAndShowGUI(String frameName) {
+        JFrame frame = new JFrame(frameName);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Menu(frame.getContentPane());
+        if (frameName == "Menu") {
+            Menu(frame.getContentPane());
+        } else if (frameName == "Setup")
+            Setup(frame.getContentPane());
+        else if (frameName == "Statistics") {
+            Statistics(frame.getContentPane());
+        } else {
+            Rules(frame.getContentPane());
+        }
 
         frame.pack();
-        frame.setSize( 600, 600 );
-        frame.setVisible(true);
-    }
-    private static void createAndShowGUISetup() {
-        JFrame frame = new JFrame("Setup");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Setup(frame.getContentPane());
-
-        frame.pack();
-        frame.setSize( 600, 600 );
-        frame.setVisible(true);
-
-    }
-    private static void createAndShowGUIRules() {
-        JFrame frame = new JFrame("Rules");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Rules(frame.getContentPane());
-
-        frame.pack();
-        frame.setSize( 600, 500 );
-        frame.setVisible(true);
-
-    }
-    private static void createAndShowGUIStats(){
-        JFrame frame = new JFrame("Statistics");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        Statistics(frame.getContentPane());
-
-
-        frame.pack();
-        frame.setSize( 600, 600 );
+        frame.setSize(600, 600);
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        createAndShowGUIMenu();
-        createAndShowGUISetup();
-        createAndShowGUIStats();
-        createAndShowGUIRules();
+    private class CheckBoxHandler  implements ActionListener{    // respond to checkbox events
+        public void actionPerformed(ActionEvent event) {
+            // process bold checkbox events
+            if (event.getSource().toString() == "PVP") {
+                playerOption = "PVP";
+            } else if (event.getSource().toString() == "PVC") {
+                playerOption = "PVC";
+            } else if (event.getSource().toString() == "CVC") {
+                playerOption = "CVC";
+            }
+
+            if (event.getSource() == "capture") {
+                gameMode = "capture";
+            } else if (event.getSource() == "avalanche") {
+                gameMode = "avalanche";
+            }
+
+        } // end method itemStateChanged
     }
+    // end private inner class CheckBoxHandler
+
+    private class ButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            if (event.equals("playGame")) {
+                createAndShowGUI("Setup");
+            } else if (event.getSource().toString().equals("stats")) {
+                createAndShowGUI("Statistics");
+            } else if (event.getSource().toString().equals("DoNotPress")) {
+                if (colorChooser == 9)
+                    colorChooser = 0;
+                else
+                    colorChooser++;
+            }
+        } // end method itemStateChanged
+
+    } // end private inner class CheckBoxHandler
+}
+//
+//    public class Butt extends JFrame implements ActionListener{
+//        private Button button1;
+//        public Butt(){
+//            super();
+//            this.setSize(100,100);
+//            this.setVisible(true);
+//
+//            this.button1 = newJButton("1");
+//            this.button1.addActionListener(this);
+//            this.add(button1);
+//        }
+//        public static void main(String[] args){
+//            Butt but = new Butt();
+//            but.setVisible(true);
+//        }
+//        public void actionPerformed(ActionEvent e){
+//            if(e.getSource() == button1){
+//
+//            }else if(e.getSource() == button2){
+//
+//            }else if(e.getSource() == button3){
+//
+//            }
+//        }
+//    }
 
 //    public menu(){
 //        setLayout(new FlowLayout());
@@ -197,4 +280,4 @@ public class menu extends JFrame{
 //        }
 
 
-}
+
