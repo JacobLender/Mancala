5fb5cb2425ebec16658e195c89f4eed25ded07ec
Jamsheedy <58054056+Jamsheedy@users.noreply.github.com>
2020-04-13 12:29:50 -0400

    Game becomes abstract. Capture mode added and works. Avalanche needs a lot of work.
    Game can detect when game is done. Will clean up peices and display winners.
    
    Navbar added to make turns more obvious. Will display message during turns and when winner is found.

--- Player.java
+++ Player.java
@@ -1,25 +1,30 @@
 public abstract class Player {
     protected int pieces;
-    protected String side;
+    protected int home;
     public Player(){
-        this("none");
+        this(0);
     }
-    public Player( String s ){
+    public Player(int h){
         pieces = 0;
-        side = s;
+        home = h;
     }
-    public abstract void takeTurn(Board board);
+    public abstract int takeTurn(Board b);
 
     //accessors
     public int getPieces()
     { return pieces; }
-
+    public int getHome()
+    { return home; }
+    public int oppHome(){
+        return home * -1;
+    }
 }
 class HumanPlayer extends Player{
-    public HumanPlayer(String s){
-        super(s);
+    public HumanPlayer(int h){
+        super(h);
     }
-    public void takeTurn(Board board){
+    public int takeTurn(Board b){
 
+        return 0;
     }
 }
